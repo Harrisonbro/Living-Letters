af10a0bc408b623cae6d9910d892b4d59886cfbf
Harrison Brown <harrisonbro@gmail.com>
2014-03-04 13:48:31 +0000

    added basic building blocks for timeline text

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php require_once('header.php'); ?>
 
 <div class="container">
-	<p>homepage...</p>
+
+	<div class="timeline">
+		
+		<div class="timeline__item">
+			<h2 class="delta">Some intro text</h2>
+			<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla.</p>
+		</div><!-- .timeline__text -->
+
+		<div class="timeline__connector"></div>
+
+		<div class="timeline__item timeline__item--narrow">
+			<h2 class="delta">Some narrow text</h2>
+			<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla.</p>
+		</div><!-- .timeline__text -->
+
+	</div><!-- .timeline -->
+
 </div><!-- .container -->
 
 <?php require_once('footer.php'); ?>
