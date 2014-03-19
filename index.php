51d4d4e531172af0932e8dea67b677db22e91c61
Harrison Brown <harrisonbro@gmail.com>
2014-03-19 12:26:30 +0000

    New annotation content from Mel

--- index.php
+++ index.php
@@ -257,10 +257,10 @@
 				you shall immediately upon the sight hereof
 				without any manner of
 				^ of further circust[ııı??ıs?] of law or^ 
-				delay cause them to be ^ ???d hanged 
+				delay cause them to be ^ ???d  
 				<span id="annotation-24" data-callout="#annotation-callout-24" class="annotation annotation-type--letters">
-				^ indicted and strait
-				thereupon arraigned and so without further traet put all to execution 
+				 <strike>indicted and strait
+								thereupon arraigned and so without further traet put all to execution</strike> hanged
 				</span>
 				as most arrant traitors / 
 				^ in such sundry places as ye shall think requisite ^ 
@@ -332,18 +332,19 @@
 		<div data-annotation="#annotation-3" id="annotation-callout-3" class="annotation-callout">
 			<div class="annotation-callout__inner">
 			<h5>"Stranger"</h5>
-			<p>This word had a number of senses in EME: a foreigner, a new arrival, a guest or vistor in a household, an unknown person or a person not of one's kin or friendship network. It's not clear which meaning is intended here. What is interesting is the use of the definite article 'the stranger', which suggests he is (contradictorily, if going with the PDE sense) known to Dutton and Cromwell. This implies perhaps the sense of a new arrival or a guest or visitor, who has been at Norton for a while, rather than someone completely unknown.</p>
+			<p>This word had a number of senses in EME: a foreigner, a new arrival, a guest or vistor in a household, an unknown person or a person not of one's kin or friendship network <small>(OED Online)</small>. It's not clear which meaning is intended here. What is interesting is the use of the definite article 'the stranger', which suggests he is (contradictorily, if going with the PDE sense) known to Dutton and Cromwell. This implies perhaps the sense of a new arrival or a guest or visitor, who has been at Norton for a while, rather than someone completely unknown.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-4" id="annotation-callout-4" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>Parenthetical or 'aside' - evocative of more spoken language. The phrase was a translation of Latin <em>deo volente</em>.</p>
+				<h4>Speeth & writing</h4>
+				<p>Parenthetical or 'aside' - evocative of more spoken language. The phrase was a translation of Latin <em>deo volente</em>.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-5" id="annotation-callout-5" class="annotation-callout">
 			<div class="annotation-callout__inner">
 			<h4>"dan"</h4>
-			<p>dan is a respectful title, equivalent to 'Sir' or 'Master'. It was particularly common to refer to those of religious orders (as here). It was adopted into English from Old French 'dan', and survives in the modern-day form 'dom', used with the same meaning, in Portugese/Brazilian-Portugese.</p>
+			<p>dan is a respectful title, equivalent to 'Sir' or 'Master'. It was particularly common to refer to those of religious orders (as here). It was adopted into English from Old French 'dan', and survives in the modern-day form 'dom', used with the same meaning, in Portugese/Brazilian-Portugese. <small>(OED Online)</small></p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-6" id="annotation-callout-6" class="annotation-callout">
@@ -354,104 +355,132 @@
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-7" id="annotation-callout-7" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>The forward slash [can't see the content in transcription notes]</p>
+			<h4>'Virgule'</h4>
+			<p>This is an early modern punctuation symbol, later replaced by the comma or full stop.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-8" id="annotation-callout-8" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>"which" [can't see the content in transcription notes]</p>
+			<h4>Relative pronouns</h4>
+			<p>In Modern English, we have different forms depending on whether the thing being referred to is alive (who) or inanimate (which). In sixteenth-century this system was not yet in place with 'which' being used for humans as well as inanimate objects.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-9" id="annotation-callout-9" class="annotation-callout">
 			<div class="annotation-callout__inner">
 			<h4>The seid abbott/the seid Monke</h4>
-			<p>The past participle of verb say is used here in the sense 'the aforementioned', 'the one referred to previously/above'. Given the length of the letter, this precision of reference is quite striking, but is a feature typical of administrative and legal language, in which specificity and accuracy was paramount. Dutton may have been familiar with producing such documents in his role as Sheriff of Cheshire. This stylistic feature is also suggested by the coordinating adverbials: moreover and wherefore. It suggests Dutton was literate and an experienced letterwriter.</p>
+			<p>'Seid' is used here in the sense 'the aforementioned', 'the one referred to previously/above'. Given the length of the letter, this precision of reference is quite striking, but is a feature typical of administrative and legal language, in which specificity and accuracy was paramount.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-10" id="annotation-callout-10" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>The address of a letter was situated on the outer side of the folded and sealed page; this was the bearer's instruction, but it also indicated to those who carried and delivered the letter the relationship between writer and addressee. Dutton stipulates Cromwell's position as Secretary to the King - which suggests Dutton was writing before (or unknowing of) Cromwell's appointment as Lord Privy Seal and Earl of Essex in 1536 [explains Calendar dating of 1534].</p>
+			<h4>Letter delivery</h4>
+			<p>'Bearer' refers to the deliver of the letter. Before the establishment of a formal post system letter writers had to find their own means of sending letters, commonly via friends or acquaintances travelling to the same destination. Bearers were often entrusted to verbally deliver further information to the recipient.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-11" id="annotation-callout-11" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>(can't find content)</p>
+			<h4>Pronoun change</h4>
+			<p>In Sixteenth Century English there were 2 forms of the pronoun you: 'you' and 'ye'. Originally they would be used to mark grammatical case but by the 1530s 'you' was starting to dominate in all contexts. This sentence means "I am telling you that what you do is your own choice".</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-12" id="annotation-callout-12" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<h4>Relative Pronouns</h4>
-			<p>In Modern English, we have different forms depending on whether the thing being referred to is alive (who) or inanimate (which). In sixteenth-century, this system was only just beginning to develop: which is why we find 'whiche [the monk] is a gud religious man'. There is an example of who, but this is used to refer to God 'our lord god who mercifully' . This reflects the origins of the who form, with God - as the entity at the top of the social hierarchy/Chain of Being - first getting an 'animate' relative marker. Over the next one hundred years, this would diffuse outwards to other types of animate being, including humans. If the letter had been written in 1635, the monk would be 'who'.</p>
+			<h4>Relative Pronouns (contd.)</h4>
+			<p>'Who' was a new relative pronoun which led to to the development of animacy marking (ie. showing what's alive or not alive). The example reflects the origins of the 'who' form which was first used to refer to God in correspondence.</p>
+			<a href="#">Read more</a>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-13" id="annotation-callout-13" class="annotation-callout">
 			<div class="annotation-callout__inner">
 			<h4>Closing Salutation Address</h4>
-			<p>The closing address 'by yours assured', is quite brief > normally we would expect the relationship between writer and addressee to be specified (e.g. Wyatt, 1536, below). This could indicate confidence on Dutton's part, both in his relationship with Cromwell, and in the persuasiveness of his letter. [If we find other letters from Dutton, we can compare this feature [found!]].</p>
+			<p>The closing address 'by yours assured', is quite brief. Normally we would expect the relationship between writer and addressee to be specified (e.g. Wyatt, 1536, below). This could indicate confidence on Dutton's part, both in his relationship with Cromwell, and in the persuasiveness of his letter.</p>
 			</div><!-- annotation-callout__inner -->
 		</div><!-- annotation-callout -->
 		<div data-annotation="#annotation-14" id="annotation-callout-14" class="annotation-callout">
 			<div class="annotation-callout__inner">
-			<p>(can't find content)</p>
+			<h4>Sir Piers Dutton</h4>
+			<p>Sir Piers was a landowner and one-time sheriff of Chester. He wrote many letters to Thomas Cromwell, suggesting that he was motivated by the power & money that could be offered by the King.</p>
 		</div></div><!-- annotation-callout -->
 		<div data-annotation="#annotation-15" id="annotation-callout-15" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Royal letters</h4>
+				<p>The structure of the letter is typical of scribal correspondence attributed to the monarch. To reflect Henry VIII's superior status, and also that the letter is drafted by a secretary, this is stated at the top of the sheet 'By the king'. This prefaces any opening address forms to the intended readers.</p>
 			</div>
 		</div>
 		<div data-annotation="#annotation-16" id="annotation-callout-16" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>The Royal 'we'</h4>
+				<p>This letter use the royal 'we', which was the use of the first-person plural to represent the (singular) monarch e.g. 'our', 'we'. This was a common feature in scribal monarchic letters which helped to give the letter authority and authenticity. The Royal We still exists, of course, although it tends to be more of a 'Presidential we' (used by figures such as Margaret Thatcher).</p>
 			</div>
 		</div>
 		<div data-annotation="#annotation-17" id="annotation-callout-17" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Spelling inconsistencies</h4>
+				<p>Spelling in the Sixteenth Century was highly variable, even for peoples' names. Despite Piers Dutton spelling his name 'Perus' the scribe uses both 'Piers' and 'Pearse'.</p>
 			</div>
 		</div>
 		<div data-annotation="#annotation-18" id="annotation-callout-18" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>The Abbots of Norton</h4>
+				<p>The Abbots of Norton had a shady past. In 1522 an inquiry found that various illegal activities had taken place including the Abbot father a child with a club-foot.</p>
 			</div>
 		</div>
 		<div data-annotation="#annotation-19" id="annotation-callout-19" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Sir William Brereton</h4>
+				<p>Sir William was another local nobleman and landowner. He seems to have had a tempestuous relationship with Piers Dutton.</p>
 			</div>
 		</div>
 		<div data-annotation="#annotation-20" id="annotation-callout-20" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Royal Seal</h4>
+				<p>Thomas Cromwell was appointed 'Keeper of the Privy Seal' in July 1536, a highly prestigious position. The 'Privy Seal' was the king's personal seal, which was applied to documents to authenticate their contents, and was thus a position of considerable trust.</p>
+				<p>[include picture]</p>
+				<a href="#">Read more</a>
 			</div>
 		</div>
 		<div data-annotation="#annotation-21" id="annotation-callout-21" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>'-eth'</h4>
+				<p>The king's scribe uses a verbal suffix '-eth' that is no longer found in Present-Day English. The modern day equivalent is '-s' e.g. it appears that, although '-eth' is sometimes associated with more formal, perhaps religious language.</p>
+				<a href="#">Read more</a>
 			</div>
 		</div>
 		<div data-annotation="#annotation-22" id="annotation-callout-22" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>What the King says vs. what the King really means</h4>
+				<p>The second-half of the letter outlines the king's commands. These are explicitly linked to the king's personal will: 'our pleasure and commandment is that', using a semi-formulaic expression found elsewhere in Henry's scribal letters. Orders given to Edward Stanley, Earl of Derby, use a comparable phrase: 'Our pleasure and commandment is, that upon the receipt hereof ye shall so prepare yourself '.</p>
+				<a href="#">Read more</a>
 			</div>
 		</div>
 		<div data-annotation="#annotation-23" id="annotation-callout-23" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Bad handwriting!</h4>
+				<p>This manuscript is not the letter that was sent to Cheshire. It's actually a draft of the text, and the number of corrections, crossings-out and insertions in the second-half of the letter are typical of this stage in the letter-writing process. The question marks shown here are illegible additions. Once the letter was approved it would be re-written to be sent.</p>
+				<a href="#">Read more</a>
+				[include zoomed in correction from photo]
 			</div>
 		</div>
 		<div data-annotation="#annotation-24" id="annotation-callout-24" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Changing his mind</h4>
+				<p>Draft letters provide a valuable insight into the letter-writing process. The modifications to the draft generally tone down and generalise the instructions. Henry could have dictated the letter in one of his infamous rages before he, or his secretaries, made the revisions to achieve a calmer, more controlled tone.</p>
+				<a href="#">Read more</a>
 			</div>
 		</div>
 		<div data-annotation="#annotation-25" id="annotation-callout-25" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Execution</h4>
+				<p>The punishments of the abbots and monks involved in the uprising were often severe. Although the King's commission acquitted some North-West canons, some were imprisoned and others were put to death by hanging and hanging in chains. The execution of another Abbot was described as "a spectacle and terror to all others".</p>
+				<a href="#">Read more</a>
 			</div>
 		</div>
 		<div data-annotation="#annotation-26" id="annotation-callout-26" class="annotation-callout">
 			<div class="annotation-callout__inner">
-				<p>content yet to be added...</p>
+				<h4>Dictation</h4>
+				<p>The sheer volume of royal correspondence required to maintain contact and convey information across the realm meant that it had long been impractical for the monarch to personally write letters. Instead, secretaries were appointed to draft and write the bulk of official correspondence, which was later authenticated by the king's signature and seal</p>
+				<a href="#">Read more</a>
+				[picture of seal]
 			</div>
 		</div>
 	</div><!-- .letter__annotations -->
