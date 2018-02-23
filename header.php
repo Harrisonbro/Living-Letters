18cdbe5a04b062daab0cd1430a48418b894582e3
Harrison Brown <harrisonbro@gmail.com>
2018-02-23 14:30:28 +0000

    Fixes typekit

--- header.php
+++ header.php
@@ -1,18 +1,18 @@
 <?php include('includes.php'); ?>
 <!DOCTYPE HTML>
-<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]--> 
-<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
-<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
-<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
+<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
 <head>
-    
+
   <title>Living Letters</title>
-  
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <!-- Apple touch icons. See http://mathiasbynens.be/notes/touch-icons -->
   <link rel="apple-touch-icon-precomposed" href="<?=SITE_IMAGES?>/apple-touch-icon-152x152-precomposed.png">
   <!-- Favicons. See http://www.jonathantneal.com/blog/understand-the-favicon/ -->
@@ -22,7 +22,7 @@
   <![endif]-->
   <meta name="msapplication-TileColor" content="#D83434">
   <meta name="msapplication-TileImage" content="<?=SITE_IMAGES?>/tileicon.png">
-  
+
   <!-- Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page -->
   <!-- See: http://www.buyog.com/code/?id=53 -->
   <meta http-equiv="x-dns-prefetch-control" content="off">
@@ -34,7 +34,7 @@
     <?php require(SITE_JS_PATH.'/typekit.js'); ?>
     <?php //require(SITE_JS_PATH.'/modernizr.custom.js'); ?>
   </script>
-    
+
 </head>
 <body>
 
--- js/typekit.js
+++ js/typekit.js
@@ -1,32 +1,29 @@
-// (function(d) {
-//   var config = {
-//     kitId: 'elw0ihn',
-//     scriptTimeout: 3000
-//   },
-//   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-// })(document);
-
 WebFontConfig = {
-  typekit: { id: 'elw0ihn' },
-  loading: function() {},
-  active: function() {
-  	jQuery(document).ready(function($) {
-		  CATH.annotations();
-		});
-  },
-  inactive: function() {
-  	jQuery(document).ready(function($) {
-		  CATH.annotations();
-		});
- }
+    typekit: { id: 'agm8rag' },
+
+    loading: function() {
+        console.log( 'Loading Typekit fonts...' );
+    },
+
+    active: function() {
+        jQuery(document).ready(function($) {
+            console.log( 'Typekit fonts loaded.' );
+            CATH.annotations();
+        });
+    },
+
+    inactive: function() {
+        jQuery(document).ready(function($) {
+            console.error( 'Typekit fonts failed.' );
+            CATH.annotations();
+        });
+    }
 };
 
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-            '://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
+(function(d) {
+    console.log( "Running webfont loader..." );
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
